ee8a4939d1a77061be0e3d9824fefde9f1e59459
qxz23pf <ciamac.davoudi@partner.bmw.de>
2024-03-13 09:40:24 +0100

    initial clean up

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident possimus eaque adipisci excepturi soluta asperiores omnis iste quibusdam itaque tempora nesciunt minima neque repellat dicta maiores, repellendus odio cumque quam.</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
