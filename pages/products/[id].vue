014cc4c913d21cdb820150dd054fc0648d80ab30
qxz23pf <ciamac.davoudi@partner.bmw.de>
2024-03-13 10:32:55 +0100

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product Details for {{ id }}</p>
+        <p>Products</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Products</p>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
