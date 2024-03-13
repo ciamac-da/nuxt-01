350ef09d27487d38615e2f7909da3df816830bfe
qxz23pf <ciamac.davoudi@partner.bmw.de>
2024-03-13 16:50:01 +0100

    error fatal

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,7 +9,7 @@ const { id } = useRoute().params
 const uri = 'https://fakestoreapi.com/products/' + id
 const { data: product } = await useFetch(uri, { key:id })
 if (!product.value) {
-    throw createError({ statusCode: 404, statusMessage: 'Product not found' })
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
   }
 
 definePageMeta({
